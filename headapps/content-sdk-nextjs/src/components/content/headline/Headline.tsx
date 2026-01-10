bf00fc18ec65d2431b9fd154805dd02588e7ea12
perezIII <rperez@crescendocollective.com>
2026-01-11 03:12:32 +0800

    test variant

--- headapps/content-sdk-nextjs/src/components/content/headline/Headline.tsx
+++ headapps/content-sdk-nextjs/src/components/content/headline/Headline.tsx
@@ -1,17 +1,60 @@
 import React from 'react';
 import { Field, Text } from '@sitecore-content-sdk/nextjs';
+import { ComponentProps } from 'lib/component-props';
 
-type HeadlineProps = {
-  fields: {
-    Headline: Field<string>;
-  };
+
+
+interface Fields {
+  Headline: Field<string>;
+}
+
+type HeadlineProps = ComponentProps & {
+  fields: Fields;
 };
 
-export const Default = (props: HeadlineProps): React.JSX.Element => {
+
+
+interface HeadlineContentProps extends HeadlineProps {
+  renderText: (fields: Fields) => React.JSX.Element;
+}
+
+const Headline = (props: HeadlineContentProps): React.JSX.Element => {
   return (
     <div className="Headline">
        {/* Editable headline */}
       <Text field={props.fields.Headline} tag="h1" />
     </div>
   );
+};
+
+
+
+
+
+export const Default = (props: HeadlineContentProps): React.JSX.Element => {
+  const renderText = (fields: Fields) => (
+    <>
+    <div className="Headline">
+       {/* Editable headline */}
+       Variant One
+      <Text field={fields.Headline} tag="h1" />
+    </div>
+    </>
+  );
+
+  return <Headline {...props} renderText={renderText} />;
+};
+
+export const AnotherVariant = (props: HeadlineContentProps): React.JSX.Element => {
+  const renderText = (fields: Fields) => (
+    <>
+       <div className="Headline">
+       {/* Editable headline */}
+       Variant Two
+      <Text field={fields.Headline} tag="h1" />
+    </div>
+    </>
+  );
+
+  return <Headline {...props} renderText={renderText} />;
 };
